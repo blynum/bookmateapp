1d389877061c53335c26916f78cade92b973c433
Belita Lynum <belita.lynum1@gmail.com>
2024-08-05 17:45:32 -0400

    Added the ability for users to press the enter key as well as the searh button to get results

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -8,12 +8,19 @@ const SearchBar = ({ onSearch }) => {
     onSearch(query);
   };
 
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <div className="search-bar">
       <input
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        onKeyDown={handleKeyDown}
         placeholder="Search for books..."
       />
       <button onClick={handleSearch}>Search</button>
